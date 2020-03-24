637c75b7496481c0eed711a4233c96f24fe65a59
Lucas Bacelar <luk_bacelar@hotmail.com>
2020-03-24 13:32:19 -0300

    Handling exceptions and claering screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,17 +29,18 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
-		String s = sc.nextLine();
-		char column = s.charAt(0);
-		int row = Integer.parseInt(s.substring(1));
-		return new ChessPosition(column, row);
-	}
-		catch(RuntimeException e) {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Erro lendo posicao de chadrez");
 		}
 	}
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -62,5 +63,14 @@ public class UI {
 			}
 		}
 		System.out.print(" ");
+	}
+
+	
+
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+		
+		
 	}
 }
